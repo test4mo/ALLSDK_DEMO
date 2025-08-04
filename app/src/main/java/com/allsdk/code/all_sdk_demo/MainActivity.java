7c82807e78ef96cc24a0ee37bcd2b4b581b1f7ea
test4mo <test4mo@gmail.com>
2025-08-04 16:29:51 +0800

    update admob sdk version

--- app/src/main/java/com/allsdk/code/all_sdk_demo/MainActivity.java
+++ app/src/main/java/com/allsdk/code/all_sdk_demo/MainActivity.java
@@ -1,8 +1,26 @@
 package com.allsdk.code.all_sdk_demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.initialization.AdapterStatus;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +28,133 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+                for (Map.Entry<String, AdapterStatus> entry : initializationStatus.getAdapterStatusMap().entrySet()) {
+                    AdapterStatus adapterStatus = entry.getValue();
+                    if (adapterStatus.getInitializationState() == AdapterStatus.State.READY) {
+                        // 初始化成功
+
+                        // 下面是广告请求展示的演示代码
+                        demoOfInterstitialAd();
+                        demoOfRewardedAd();
+                    } else {
+                        // 间隔点事件重试初始化
+                    }
+                }
+            }
+        });
+    }
+
+    private void demoOfRewardedAd() {
+        // 广告位级别的请求对象
+        AdRequest adRequest = new AdRequest.Builder().build();
+        RewardedAd.load(this, "激励视频广告ID", adRequest, new RewardedAdLoadCallback() {
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+
+
+                // 可以使用同一个 adRequest 重试几次, 如果切换 adRequest 对象会导致请求数偏高，填充率偏低
+                // 重试请求广告，注意控制总次数和重试间隔, 比如最大5次，每次间隔1分钟
+            }
+
+            @Override
+            public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                super.onAdLoaded(rewardedAd);
+
+                rewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                    @Override
+                    public void onAdClicked() {
+                        super.onAdClicked();
+                    }
+
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        super.onAdDismissedFullScreenContent();
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                        super.onAdFailedToShowFullScreenContent(adError);
+                    }
+
+                    @Override
+                    public void onAdImpression() {
+                        super.onAdImpression();
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        super.onAdShowedFullScreenContent();
+                    }
+                });
+
+                rewardedAd.show(MainActivity.this, new OnUserEarnedRewardListener() {
+                    @Override
+                    public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                        // 给用户颁发激励 , 虚拟币，道具等
+                    }
+                });
+            }
+        });
+    }
+
+    private void demoOfInterstitialAd() {
+        // 广告位级别的请求对象
+        AdRequest adRequest = new AdRequest.Builder().build();
+        //  InterstitialAd interstitialAd =
+
+        // 请求插页广告
+        InterstitialAd.load(this, "插页广告位ID", adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+
+                // 可以使用同一个 adRequest 重试几次, 如果切换 adRequest 对象会导致请求数偏高，填充率偏低
+                // 重试请求广告，注意控制总次数和重试间隔, 比如最大5次，每次间隔1分钟
+            }
+
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                super.onAdLoaded(interstitialAd);
+
+                // 请求到广告
+
+                // 设置广告展示流程的监听回调
+                interstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                    @Override
+                    public void onAdClicked() {
+                        super.onAdClicked();
+                    }
+
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        super.onAdDismissedFullScreenContent();
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                        super.onAdFailedToShowFullScreenContent(adError);
+                    }
+
+                    @Override
+                    public void onAdImpression() {
+                        super.onAdImpression(); // 成功曝光
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        super.onAdShowedFullScreenContent();
+                    }
+                });
+
+                // 展示广告
+                interstitialAd.show(MainActivity.this);
+            }
+        });
     }
 }
